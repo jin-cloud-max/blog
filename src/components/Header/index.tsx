6f921021005689a22abcbb56d1d2cfb9cae6af18
Jin Bok de Oliveira <jinoliveira74@gmail.com>
2021-05-30 13:01:51 -0300

    chore: header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,20 @@
-export default function Header() {
-  // TODO
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+
+import Link from 'next/link';
+
+import styles from './header.module.scss';
+
+export function Header() {
+   return (
+      <header className={styles.container}>
+         <div className={styles.content}>
+            <Link href="/">
+               <a>
+                  <img src="/images/Logo.svg" alt="spacetraveling" />
+               </a>
+            </Link>
+         </div>
+      </header>
+   );
 }
